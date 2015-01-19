a73f91ed95fe1d2d1381f95a9e98812619898d56
dramaeasy <screamo14@aim.com>
2015-01-18 18:29:30 -0800

    Testing the push of Level 01

---
+++ Assets/Scripts/Player/Flashlight.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+
+	Light flashlightSource;
+	bool lightOn = true;
+	public float drainPower = 0.1f;
+	public float batteryLife = 0.0f;
+	public float maxBatteryLife = 2.0f;
+
+	AudioSource turnOn;
+	AudioSource turnOff;
+
+	// Use this for initialization
+	void Start () {
+		batteryLife = maxBatteryLife;
+		flashlightSource = GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(batteryLife >= 0 && lightOn)
+		{
+			batteryLife -= drainPower * Time.deltaTime;
+		}
+
+		flashlightSource.intensity = batteryLife;
+
+		if(batteryLife <= 0)
+		{
+			lightOn = false;
+		}
+
+		if(Input.GetKeyDown(KeyCode.F))
+		{
+			//TODO play flash light sounds
+			toggleFlashlight();
+
+			if(lightOn)
+			{
+				lightOn = false;
+			}
+			else if(batteryLife >= 0)
+			{
+				lightOn = true;
+			}
+		}
+
+
+
+	
+	}
+
+	void toggleFlashlight()
+	{
+		if (lightOn) 
+		{
+			flashlightSource.enabled = false;
+		}
+		else
+		{
+			flashlightSource.enabled = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerFootsteps.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFootsteps : MonoBehaviour {
+
+	public AudioClip ground;
+
+	bool step = true;
+	bool playing = false;
+	float audioLength = 0.3f;
+	float elapTime = 0f;
+
+	void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		CharacterController controller = GetComponent<CharacterController> ();
+
+		if (controller.isGrounded && controller.velocity.magnitude < 7 && 
+						controller.velocity.magnitude > 5 && hit.gameObject.tag == "Ground" && step == true ||
+		    controller.isGrounded && controller.velocity.magnitude < 7 && 
+		    controller.velocity.magnitude > 5 && hit.gameObject.tag == "Untagged" && step == true) 
+		{
+			walkOnGround();
+		}
+	}
+
+	void walkOnGround()
+	{
+		/*
+		if (!playing) 
+		{
+			step = false;
+			playing = true;
+			elapTime = 0f;
+			audio.clip = ground;
+			audio.Play ();
+			step = true;
+		}
+		*/
+
+		if(!audio.isPlaying)
+		{
+			audio.clip = ground;
+			audio.Play ();
+		}
+		
+	}
+
+	void Update()
+	{
+		if(playing)
+		{
+			elapTime += Time.deltaTime;
+			if(elapTime >= audioLength)
+			{
+				playing = false;
+			}
+		}
+	}
+	
+}
